970b2e8f3b31a1446d407b5aca32331cb2ccbc73
wjcrumbleholme <64094813+wjcrumbleholme@users.noreply.github.com>
2023-03-10 20:11:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,28 @@ from flet.plotly_chart import PlotlyChart
 
 def main(page: ft.Page):
 
+    page.title = "Bogo-to-infinity"
+    page.window_height = 1024
+    page.window_width = 1440
+
+
     fig = go.Figure()
 
     fig.add_trace(go.Box(
-    y=[0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15,
+    x=[0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15,
        8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
-    name="Only Whiskers",
-    boxpoints='all',
-    points='all', # no data points
+    name='.',
+    boxpoints=False, # no data points
     marker_color='rgb(9,56,125)',
     line_color='rgb(9,56,125)'
     ))
 
-    page.add(PlotlyChart(fig, expand=True))
+    page.add(
+        ft.Row(
+            PlotlyChart(fig, expand=True)
+        )
+
+        
+        )
 
 ft.app(target=main)
