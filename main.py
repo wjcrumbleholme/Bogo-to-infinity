613285500f653259d21dd0d8e8842d9f6cab13d4
Zoobdude <96008479+Zoobdude@users.noreply.github.com>
2023-03-10 17:22:32 +0000

    Co-authored-by: wjcrumbleholme <wjcrumbleholme@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,77 +1,25 @@
-from random import shuffle
-from colorama import Fore, Back, Style
-import time
-# start_time=time.time()
+import plotly.graph_objects as go
 
-def makeNewList(length):
-    global list_shuffled, list_of_num
-    list_of_num = []
-    list_of_num = list(range(1, length + 1))
-    list_shuffled = list_of_num[:]
-    shuffle(list_shuffled)
-
-def shuffleList (list):
-    shuffle(list)
-
-def alikeness(list1):
-    total_alikeness = 0
-    for i in range(0, len(list1) - 1):
-        total_alikeness += abs(list_shuffled[i] - (i+1))
-    alikecol(total_alikeness, list1)
-    
-
-def alikecol(total_alikeness, list1):
-    sumTotal = sum(list_of_num) 
-    lq = sumTotal / 100 * 25
-    mid = sumTotal / 100 * 45 
-    uq = sumTotal / 100 * 65
-    
-    #lower alikeness is better
-    if total_alikeness > uq:
-        print(Fore.RED + str(list1))
-    elif total_alikeness > mid:
-        print(Fore.YELLOW + str(list1))
-    elif total_alikeness > lq:
-        print(Fore.GREEN + str(list1))
-    else:
-        print(Fore.CYAN + str(list1))
+import flet as ft
+from flet.plotly_chart import PlotlyChart
 
 
-def sortChecker(list1):
-    sorted = False
-    temp = 0
-    while sorted == False:
-        sorted = True
-        for i in range(0, len(list1) - 1):
-            if list1[i+1] < list1[i]:
-                sorted = False
-        alikeness(list1)
-        shuffleList(list1)
+def main(page: ft.Page):
 
+    fig = go.Figure()
 
-current_list_length = 7
-makeNewList(current_list_length)
-t1 = time.time()
-sortChecker(list_shuffled)
-t2 = time.time()-t1
-print(f"sortChecker took {t2} seconds")
+    fig = go.Figure()
+    fig.add_trace(go.Box(
+        y=[0.75, 5.25, 5.5, 6, 6.2, 6.6, 6.80, 7.0, 7.2, 7.5, 7.5, 7.75, 8.15,
+        8.15, 8.65, 8.93, 9.2, 9.5, 10, 10.25, 11.5, 12, 16, 20.90, 22.3, 23.25],
+        name="All Points",
+        jitter=0.3,
+        pointpos=-1.8,
+        boxpoints='all', # represent all points
+        marker_color='rgb(7,40,89)',
+        line_color='rgb(7,40,89)'
+    ))
 
+    page.add(PlotlyChart(fig, expand=True))
 
-# while True:
-#     sort_start_time=time.time()
-#     while list_shuffled != list_of_num:
-#         makeNewList(current_list_length)
-#         alikeness()
-#     time_taken = time.time() - sort_start_time
-#     print(list_of_num)
-#     print(list_shuffled)
-#     print("Time taken to sort list of length " + str(current_list_length) + " is " + str(time_taken))
-#     current_list_length += 1
-#     makeNewList(current_list_length)
-        
-makeNewList(10)
-
-#---TODO---
-# Previous times for sort + 1
-# Current time (stopwatch)
-# Total running time
+ft.app(target=main,view=ft.WEB_BROWSER)
